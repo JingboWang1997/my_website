595ce606079fbb80fe875113117eda8826dd2075
Jingbo Wang <jingbowang@lawn-128-61-10-160.lawn.gatech.edu>
2020-06-03 23:43:41 -0400

    Updates

---
+++ precache-manifest.a7e7417993a34552ef00d2d43df59033.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad27b12331f08b7ef3e2c1931b96964",
+    "url": "/my_website/index.html"
+  },
+  {
+    "revision": "8a23f5275d0e88cc04dc",
+    "url": "/my_website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b973a907ad47df039752",
+    "url": "/my_website/static/js/2.83b31707.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/my_website/static/js/2.83b31707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a23f5275d0e88cc04dc",
+    "url": "/my_website/static/js/main.8a9fba6f.chunk.js"
+  },
+  {
+    "revision": "235a46e0dd636782d0bb",
+    "url": "/my_website/static/js/runtime-main.1dbd6394.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_website/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my_website/precache-manifest.09c10780bd8fcdb5aaf7f2ffdc51b09f.js"
+  "/my_website/precache-manifest.a7e7417993a34552ef00d2d43df59033.js"
 );
 
 self.addEventListener('message', (event) => {
